b3b1d681cb6b7c914461fe479583eb8cc952f927
korea8378 <korea8378@naver.com>
2018-11-14 15:04:33 +0900

    add : Add springboot-WebMVC-2

--- ldj-spring-boot-webmvc/src/main/java/ldj/spring/boot/webmvc/config/WebConfig.java
+++ ldj-spring-boot-webmvc/src/main/java/ldj/spring/boot/webmvc/config/WebConfig.java
@@ -1,9 +1,16 @@
 package ldj.spring.boot.webmvc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override //기존의 핸들러를 유지하면서 핸들러를 정의해서 추가하겠다.
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")
+                .addResourceLocations("classpath:/m/")
+                .setCachePeriod(20);
+    }
 }
