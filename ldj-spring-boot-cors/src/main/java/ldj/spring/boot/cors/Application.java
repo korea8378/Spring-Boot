5c308c7d5b095ad5dc9107903a3cab7be8af2c7a
korea8378 <korea8378@naver.com>
2018-11-19 19:38:25 +0900

    add : Add cors

---
+++ ldj-spring-boot-cors/src/main/java/ldj/spring/boot/cors/Application.java
@@ -0,0 +1,22 @@
+package ldj.spring.boot.cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+//    @CrossOrigin(origins = "http://localhost:18080")
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
