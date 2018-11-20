acd9ab462f7654264d7e531de55c80faeabc3425
korea8378 <korea8378@naver.com>
2018-11-20 16:14:07 +0900

    add : Add springboot-data-h2

---
+++ ldj-spring-boot-h2/src/main/java/ldj/spring/boot/h2/ldjspringbooth2/LdjSpringBootH2Application.java
@@ -0,0 +1,12 @@
+package ldj.spring.boot.h2.ldjspringbooth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdjSpringBootH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdjSpringBootH2Application.class, args);
+	}
+}
