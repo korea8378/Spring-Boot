976f12af373d4310f391f76f63b9217fa4f199b1
korea8378 <korea8378@naver.com>
2018-11-21 21:44:06 +0900

    add : Add springboot-data-neo4j

---
+++ ldj-spring-boot-neo4j/src/main/java/ldj/spring/boot/neo4j/account/AccountRepository.java
@@ -0,0 +1,6 @@
+package ldj.spring.boot.neo4j.account;
+
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+public interface AccountRepository extends Neo4jRepository<Account, Long> {
+}
